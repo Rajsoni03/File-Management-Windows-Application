9d675a83d48a3e94bccd70f26f141157f1526864
Rajkumar Soni <rajksoni029@gmail.com>
2022-02-14 17:08:06 +0530

    duplicates search feature added

--- File Management Tool/MainForm.h
+++ File Management Tool/MainForm.h
@@ -1,7 +1,6 @@
 #pragma once
 
 namespace FileManagementTool {
-
 	using namespace System;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
@@ -23,6 +22,12 @@ namespace FileManagementTool {
 			//
 			//TODO: Add the constructor code here
 			//
+
+			folderCount = 0;
+			fileCount = 0;
+			folderCountDuplicate = 0;
+			fileCountDuplicate = 0;
+			
 		}
 
 	protected:
@@ -45,15 +50,26 @@ namespace FileManagementTool {
 
 	private: System::Windows::Forms::Label^ Result;
 	private: System::Windows::Forms::Button^ selectBtn;
+	private: System::Windows::Forms::Label^ result1;
 
 
-	private: System::Windows::Forms::Label^ duplicates;
+
 	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
 	private: System::Windows::Forms::Button^ showBtn;
-	private: System::Windows::Forms::ListBox^ listBox;
+	private: System::Windows::Forms::ListBox^ listBox1;
 
 
+	private: System::Collections::Hashtable^ mytable;
+	private: int folderCount;
+	private: int fileCount;
+	private: int folderCountDuplicate;
+	private: int fileCountDuplicate;
 
+	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel2;
+	private: System::Windows::Forms::ListBox^ listBox2;
+	private: System::Windows::Forms::Label^ result2;
+	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel3;
+	private: System::Windows::Forms::ListBox^ listBox3;
 
 
 
@@ -76,11 +92,18 @@ namespace FileManagementTool {
 			this->directoryName = (gcnew System::Windows::Forms::TextBox());
 			this->Result = (gcnew System::Windows::Forms::Label());
 			this->selectBtn = (gcnew System::Windows::Forms::Button());
-			this->duplicates = (gcnew System::Windows::Forms::Label());
+			this->result1 = (gcnew System::Windows::Forms::Label());
 			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
 			this->showBtn = (gcnew System::Windows::Forms::Button());
-			this->listBox = (gcnew System::Windows::Forms::ListBox());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->tableLayoutPanel2 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->tableLayoutPanel3 = (gcnew System::Windows::Forms::TableLayoutPanel());
+			this->listBox3 = (gcnew System::Windows::Forms::ListBox());
+			this->listBox2 = (gcnew System::Windows::Forms::ListBox());
+			this->result2 = (gcnew System::Windows::Forms::Label());
 			this->tableLayoutPanel1->SuspendLayout();
+			this->tableLayoutPanel2->SuspendLayout();
+			this->tableLayoutPanel3->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// label1
@@ -102,7 +125,7 @@ namespace FileManagementTool {
 			this->directoryName->Location = System::Drawing::Point(3, 3);
 			this->directoryName->Multiline = true;
 			this->directoryName->Name = L"directoryName";
-			this->directoryName->Size = System::Drawing::Size(446, 19);
+			this->directoryName->Size = System::Drawing::Size(686, 19);
 			this->directoryName->TabIndex = 1;
 			// 
 			// Result
@@ -119,22 +142,24 @@ namespace FileManagementTool {
 			this->selectBtn->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
 				| System::Windows::Forms::AnchorStyles::Left)
 				| System::Windows::Forms::AnchorStyles::Right));
-			this->selectBtn->Location = System::Drawing::Point(455, 3);
+			this->selectBtn->Location = System::Drawing::Point(695, 3);
 			this->selectBtn->Name = L"selectBtn";
-			this->selectBtn->Size = System::Drawing::Size(108, 19);
+			this->selectBtn->Size = System::Drawing::Size(168, 19);
 			this->selectBtn->TabIndex = 4;
 			this->selectBtn->Text = L"Select Folder";
 			this->selectBtn->UseVisualStyleBackColor = true;
 			this->selectBtn->Click += gcnew System::EventHandler(this, &MainForm::searchBtn_Click);
 			// 
-			// duplicates
+			// result1
 			// 
-			this->duplicates->AutoSize = true;
-			this->duplicates->Location = System::Drawing::Point(12, 112);
-			this->duplicates->Name = L"duplicates";
-			this->duplicates->Size = System::Drawing::Size(55, 13);
-			this->duplicates->TabIndex = 0;
-			this->duplicates->Text = L"duplicates";
+			this->result1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left));
+			this->result1->AutoSize = true;
+			this->result1->Location = System::Drawing::Point(3, 0);
+			this->result1->Name = L"result1";
+			this->result1->Size = System::Drawing::Size(38, 27);
+			this->result1->TabIndex = 0;
+			this->result1->Text = L"result1";
 			// 
 			// tableLayoutPanel1
 			// 
@@ -151,7 +176,7 @@ namespace FileManagementTool {
 			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
 			this->tableLayoutPanel1->RowCount = 1;
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
-			this->tableLayoutPanel1->Size = System::Drawing::Size(566, 25);
+			this->tableLayoutPanel1->Size = System::Drawing::Size(866, 25);
 			this->tableLayoutPanel1->TabIndex = 5;
 			// 
 			// showBtn
@@ -164,26 +189,98 @@ namespace FileManagementTool {
 			this->showBtn->UseVisualStyleBackColor = true;
 			this->showBtn->Click += gcnew System::EventHandler(this, &MainForm::showBtn_Click);
 			// 
-			// listBox
+			// listBox1
 			// 
-			this->listBox->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+			this->listBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
 				| System::Windows::Forms::AnchorStyles::Left)
 				| System::Windows::Forms::AnchorStyles::Right));
-			this->listBox->FormattingEnabled = true;
-			this->listBox->Location = System::Drawing::Point(15, 150);
-			this->listBox->Name = L"listBox";
-			this->listBox->Size = System::Drawing::Size(566, 160);
-			this->listBox->TabIndex = 7;
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->Location = System::Drawing::Point(3, 30);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(425, 355);
+			this->listBox1->TabIndex = 7;
+			// 
+			// tableLayoutPanel2
+			// 
+			this->tableLayoutPanel2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->tableLayoutPanel2->ColumnCount = 2;
+			this->tableLayoutPanel2->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				50)));
+			this->tableLayoutPanel2->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				50)));
+			this->tableLayoutPanel2->Controls->Add(this->tableLayoutPanel3, 1, 1);
+			this->tableLayoutPanel2->Controls->Add(this->listBox1, 0, 1);
+			this->tableLayoutPanel2->Controls->Add(this->result2, 1, 0);
+			this->tableLayoutPanel2->Controls->Add(this->result1, 0, 0);
+			this->tableLayoutPanel2->Location = System::Drawing::Point(15, 117);
+			this->tableLayoutPanel2->Name = L"tableLayoutPanel2";
+			this->tableLayoutPanel2->RowCount = 2;
+			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 6.905371F)));
+			this->tableLayoutPanel2->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 93.09463F)));
+			this->tableLayoutPanel2->Size = System::Drawing::Size(863, 391);
+			this->tableLayoutPanel2->TabIndex = 8;
+			// 
+			// tableLayoutPanel3
+			// 
+			this->tableLayoutPanel3->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->tableLayoutPanel3->ColumnCount = 1;
+			this->tableLayoutPanel3->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
+				50)));
+			this->tableLayoutPanel3->Controls->Add(this->listBox3, 0, 1);
+			this->tableLayoutPanel3->Controls->Add(this->listBox2, 0, 0);
+			this->tableLayoutPanel3->Location = System::Drawing::Point(434, 30);
+			this->tableLayoutPanel3->Name = L"tableLayoutPanel3";
+			this->tableLayoutPanel3->RowCount = 2;
+			this->tableLayoutPanel3->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
+			this->tableLayoutPanel3->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
+			this->tableLayoutPanel3->Size = System::Drawing::Size(426, 358);
+			this->tableLayoutPanel3->TabIndex = 9;
+			// 
+			// listBox3
+			// 
+			this->listBox3->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->listBox3->FormattingEnabled = true;
+			this->listBox3->Location = System::Drawing::Point(3, 182);
+			this->listBox3->Name = L"listBox3";
+			this->listBox3->Size = System::Drawing::Size(420, 173);
+			this->listBox3->TabIndex = 9;
+			// 
+			// listBox2
+			// 
+			this->listBox2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left)
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->listBox2->FormattingEnabled = true;
+			this->listBox2->Location = System::Drawing::Point(3, 3);
+			this->listBox2->Name = L"listBox2";
+			this->listBox2->Size = System::Drawing::Size(420, 173);
+			this->listBox2->TabIndex = 8;
+			// 
+			// result2
+			// 
+			this->result2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+				| System::Windows::Forms::AnchorStyles::Left));
+			this->result2->AutoSize = true;
+			this->result2->Location = System::Drawing::Point(434, 0);
+			this->result2->Name = L"result2";
+			this->result2->Size = System::Drawing::Size(38, 27);
+			this->result2->TabIndex = 9;
+			this->result2->Text = L"result2";
 			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(593, 333);
-			this->Controls->Add(this->listBox);
+			this->ClientSize = System::Drawing::Size(893, 520);
+			this->Controls->Add(this->tableLayoutPanel2);
 			this->Controls->Add(this->showBtn);
 			this->Controls->Add(this->tableLayoutPanel1);
-			this->Controls->Add(this->duplicates);
 			this->Controls->Add(this->Result);
 			this->Controls->Add(this->label1);
 			this->MinimumSize = System::Drawing::Size(298, 243);
@@ -192,6 +289,9 @@ namespace FileManagementTool {
 			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
 			this->tableLayoutPanel1->ResumeLayout(false);
 			this->tableLayoutPanel1->PerformLayout();
+			this->tableLayoutPanel2->ResumeLayout(false);
+			this->tableLayoutPanel2->PerformLayout();
+			this->tableLayoutPanel3->ResumeLayout(false);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -205,28 +305,42 @@ namespace FileManagementTool {
 		this->directoryName->Text = dirPath;
 	}
 	private: System::Void MainForm_Load(System::Object^ sender, System::EventArgs^ e) {
-		this->duplicates->Text = "";
+		this->result1->Text = "";
+		this->result2->Text = "";
 	}
-	private: int getFileAndFolders(String^ dirPath) {
+	private: void getFileAndFolders(String^ dirPath) {
 		auto files = Directory::GetFiles(dirPath);
 		auto folders = Directory::GetDirectories(dirPath);
 
 		int numFiles = files->Length;
 		int numFolders = folders->Length;
+		fileCount += numFiles;
+		folderCount += numFolders;
 
-		for (int i = 0; i < numFiles; i++)
-			this->listBox->Items->Add(files[i]);
-		for (int i = 0; i < numFolders; i++) {
-			this->listBox->Items->Add(folders[i]);
-			numFiles += getFileAndFolders(folders[i]);
+		for (int i = 0; i < numFiles; i++){
+			if (this->listBox1->Items->Contains(FileInfo(files[i]).Name)) {
+				//this->mytable[FileInfo(files[i]).Name] = this->mytable[FileInfo(files[i]).Name] + 1;
+				this->listBox2->Items->Add(files[i]);
+				this->listBox3->Items->Add(FileInfo(files[i]).Name);
+				fileCountDuplicate++;
+			};
+			// this->mytable->Add(FileInfo(files[i]).Name, 1);
+			this->listBox1->Items->Add(FileInfo(files[i]).Name);
 		}
-		return numFiles;
+		for (int i = 0; i < numFolders; i++) {
+			this->listBox1->Items->Add(folders[i]);
+			getFileAndFolders(folders[i]);
+		}
+		return;
 	}
 	private: System::Void showBtn_Click(System::Object^ sender, System::EventArgs^ e) {
-		listBox->Items->Clear();
+		listBox1->Items->Clear();
+		listBox2->Items->Clear();
+		listBox3->Items->Clear();
 		String^ dirPath = this->directoryName->Text;
-		int AllNumFiles = getFileAndFolders(dirPath);
-		this->duplicates->Text = "No. of Files : " + Convert::ToString(AllNumFiles);
+		getFileAndFolders(dirPath);
+		this->result1->Text = "No. of Files : " + Convert::ToString(fileCount) + "\nNo. of Folders : " + Convert::ToString(folderCount);
+		this->result2->Text = "No. of Duplicate Files : " + Convert::ToString(fileCountDuplicate);
 	}
 };
 }
